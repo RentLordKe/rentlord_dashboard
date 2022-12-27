4354f719195697a1fce55c404c6e7b437d262234
maxwellwachira <maxwellwachira67@gmail.com>
2022-12-28 00:35:10 +0300

    app & document pages

---
+++ src/pages/_app.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import type { AppProps } from 'next/app';
+import { ColorScheme, MantineProvider } from '@mantine/core';
+
+import '../styles/globals.css';
+import PageLoader from '../components/pageLoader/pageLoader';
+import { AuthContextProvider } from '../features/authentication/context/authContextProvider';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false);
+  const colorScheme: ColorScheme = "light";
+
+  useEffect(() => {
+    setTimeout(() => setLoading(true), 700);
+  }, []);
+
+  return (
+    <AuthContextProvider>
+      <MantineProvider
+        theme={{
+          colorScheme,
+          fontFamily: 'Montserrat, sans-serif'
+        }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+          {!loading ?
+            (<PageLoader />) :
+            (<Component {...pageProps} />)
+          }
+      </MantineProvider>
+    </AuthContextProvider>
+  );
+
+}
+
+export default MyApp;
