aa38ce0f541427d778fcada4c2f8aea6a045875b
maxwellwachira <maxwellwachira67@gmail.com>
2022-12-29 01:35:25 +0300

    changed body background to light gray

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { colors } from '../constants/colors';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{ background: colors.lightGrayBackground}}>
         <Main />
         <NextScript />
       </body>
